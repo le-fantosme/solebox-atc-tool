6f296b49b0d1683f8c938dd1fd90b67f79069d47
what you ain't <42221876+pxtrck@users.noreply.github.com>
2019-01-05 17:39:21 +0100

    Updated checkout after Solebox patched previous method
    Updated checkout after Solebox patched previous method

--- kidshavefunwiththis.py
+++ kidshavefunwiththis.py
@@ -9,9 +9,9 @@ import time
 print("A 5 minutes trash by pxtrck")
 s=requests.session()
 header={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
-solename=""	#WRITE THERE YOUR NAME; must be the one you used to register on solebox
-productlink=""	#the link of your product
-aid="" 	#the aid value, read README.md to check how to find it out
+solename="Patrick"	#WRITE THERE YOUR NAME; must be the one you used to register on solebox
+productlink="https://www.solebox.com/Footwear/Running/Moon-Racer-variant.html"	#the link of your product
+aid="31764" 	#the aid value, read README.md to check how to find it out
 def get_prod():
     a=s.get(productlink, headers=header)
     if a.status_code ==200:
@@ -62,8 +62,8 @@ def login_checkout():
     "actcontrol": "account",
     "fnc": "login_noredirect",
     "cl": "account",
-    "lgn_usr": "",	#YOUR SOLEBOX EMAIL, enter it there 
-    "lgn_pwd": ""}	#YOUR SOLEBOX PASSWORD 
+    "lgn_usr": "patrickforzainter@hotmail.com",	#YOUR SOLEBOX EMAIL, enter it there 
+    "lgn_pwd": "patrick2001"}	#YOUR SOLEBOX PASSWORD 
     login=s.post("https://www.solebox.com/index.php?", headers=header, data=ldt)
     if solename in login.text:
         print("Logged in")
@@ -74,8 +74,14 @@ def login_checkout():
         time.sleep(1)
         login_checkout()
 
-    conclude=("http://solebox.com/index.php?actcontrol=payment&cl=payment&fnc=validatepayment&paymentid=globalpaypal&lang=0&stoken=&userform=")
-    webbrowser.open(conclude)
+    conclude=s.get("http://solebox.com/index.php?actcontrol=payment&cl=payment&fnc=validatepayment&paymentid=globalpaypal&lang=0&stoken=&userform=", headers=header)
+    print("Waiting on PayPal")
+    time.sleep(1)
+    print(conclude.url)
+    print(datetime.datetime.now())
+    x=conclude.url
+
+    webbrowser.open(x)
     print("Opening browser, pay with PayPal. ")
     print(datetime.datetime.now())
 login_checkout()
